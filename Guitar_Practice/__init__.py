b9bb774724372eab276d89bbdf42a97675661796
jobads <jonas.baumer95@gmail.com>
2026-06-23 19:05:30 +0200

    separate sqllite db

--- Guitar_Practice/__init__.py
+++ Guitar_Practice/__init__.py
@@ -11,7 +11,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////app/data/app.db'
 
     app.app_context().push()
 
